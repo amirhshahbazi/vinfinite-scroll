d02bc5b684bb38b5c54d6ca07e7219cbf500bf40
= <rballshahbazi2@gmail.com>
2022-01-20 20:18:09 +0330

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VinfiniteScroll from '@/vinfinite-scroll.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VinfiniteScroll
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vinfinite-scroll />
+  </div>
+</template>
---
+++ src/vinfinite-scroll.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default /*#__PURE__*/{
+  name: 'VinfiniteScroll',
+  data() {
+
+  },
+  mounted() {
+
+  }
+};
+</script>
+
+<template>
+  <div class="vinfinite-scroll">
+  </div>
+</template>
+
+<style scoped>
+</style>
